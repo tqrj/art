a1e1fa82610993838fd3d19df75ff6a8648f972c
tsli <2816869721@qq.com>
2020-10-01 00:52:49 +0800

    ''

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 namespace art;
 
 use Swoole\Coroutine\Server;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
 use Swoole\Process\Pool;
 use Swoole\Redis;
 
@@ -11,15 +13,14 @@ require 'vendor/autoload.php';
 //多进程管理模块
 $pool = new Pool(2);
 //让每个OnWorkerStart回调都自动创建一个协程
-$pool->set(['enable_coroutine' => true]);
+$pool->set(['enable_coroutine' => true,'daemonize'=>1]);
 $pool->on('workerStart', function ($pool, $id) {
     //每个进程都监听9501端口
     $server = new \Swoole\Coroutine\Http\Server('0.0.0.0', '9502' , false, true);
-
     $server->handle('/', function ($request, $response) {
         $response->end("<h1>Index</h1>");
     });
-    $server->handle('/test', function ($request, $response) {
+    $server->handle('/test', function (Request $request, Response $response) {
         $response->end("<h1>Test</h1>");
     });
     $server->handle('/stop', function ($request, $response) use ($server) {
