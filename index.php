d5d1f9060e3b9df93851c10123b38cc82b2b4863
tsli <2816869721@qq.com>
2020-10-01 16:35:40 +0800

    ''

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ namespace art;
 
 use Swoole\Coroutine\Http\Request;
 use Swoole\Coroutine\Server;
-use Swoole\Http\Response;
 use Swoole\Process\Pool;
 use Swoole\Redis;
 
@@ -18,10 +17,9 @@ $pool->on('workerStart', function ($pool, $id) {
     //每个进程都监听9501端口
     $server = new \Swoole\Coroutine\Http\Server('0.0.0.0', '9502' , false, true);
     $server->handle('/', function (Request $request, $response) {
-        echo $request->path;
-        $response->end("<h1>Index</h1>");
+        $response->end(test());
     });
-    $server->handle('/test', function (Request $request, Response $response) {
+    $server->handle('/test', function (Request $request, $response) {
         $response->end("<h1>Test</h1>");
     });
     $server->handle('/stop', function (Request $request, $response) use ($server) {
