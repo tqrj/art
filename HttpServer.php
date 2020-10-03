48c796eb7088bc950e5bbede7ff3bbbb6431b259
tsli <2816869721@qq.com>
2020-10-03 10:20:32 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -19,7 +19,7 @@ $pool->set(['enable_coroutine' => true]);
 $pool->on('workerStart', function ($pool, $id) {
     //每个进程都监听9501端口
     $server = new Server('0.0.0.0', '9502' , false, true);
-    $server->handle('/', function (Request $request,Response $response) use($appHttp) {
+    $server->handle('/', function (Request $request,Response $response) {
         //有优化空间 使用context来管理 变成单例，不用每次加载
          HttpApp::init($request,$response);
          HttpApp::run();
