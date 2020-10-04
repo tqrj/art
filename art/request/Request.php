0a903f97cb9a8707e57268d58f4663b989ea35ef
tsli <2816869721@qq.com>
2020-10-04 15:57:07 +0800

    ''

--- art/request/Request.php
+++ art/request/Request.php
@@ -27,17 +27,11 @@ class Request
     public static function only(array $keys):array
     {
          $request = Context::get('request');
-         $post = $request->post;
+         $post = $request;
          $get = $request->get;
-         $params = [];
-
-         if (!is_null($post) && !is_null($get)){
-             $params = array_merge($get,$post);
-         }elseif (!is_null($post)){
-             $params = $post;
-         }elseif(!is_null($get)){
-             $params = $get;
-         }
+         is_null($post)?$post=[]:true;
+         is_null($get)?$get=[]:true;
+         $params = array_merge($get,$post);
          $result = [];
          if (count($params) == 0){
              return $result;
