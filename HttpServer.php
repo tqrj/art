5811cd4b0fed249436d3469b160ee284c7f8eb58
tsli <2816869721@qq.com>
2020-10-05 13:17:04 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace art;
 
 
-
+use art\exception\ClassNotFoundException;
+use art\exception\HttpException;
 use Swoole\Coroutine\Http\Server;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
@@ -18,23 +20,25 @@ $pool = new Pool(swoole_cpu_num() + 2);
 $pool->set(['enable_coroutine' => true]);
 $pool->on('workerStart', function ($pool, $id) {
     //每个进程都监听9501端口
-    $server = new Server('0.0.0.0', '9502' , false, true);
-    $server->handle('/', function (Request $request,Response $response) {
+    $server = new Server('0.0.0.0', '9502', false, true);
+    $server->handle('/', function (Request $request, Response $response) {
         //有优化空间 使用context来管理 变成单例，不用每次加载  已经处理，全部换成静态的方法了 使用context管理上下文
         try {
-            HttpApp::init($request,$response);
+            HttpApp::init($request, $response);
             HttpApp::run();
-        }catch (\Exception $e){
-            art_assign(202,$e->getMessage());
+        } catch (HttpException $e) {
+            art_assign($e->getCode(), $e->getMessage());
+        } catch (ClassNotFoundException $e) {
+            art_assign($e->getCode(), $e->getMessage());
         }
 
     });
-    $server->handle('/favicon.ico',function (Request $request,Response $response){
+    $server->handle('/favicon.ico', function (Request $request, Response $response) {
         $response->end('');
     });
     $server->start();
 });
-$pool->on('onWorkerStart',function ($pool,$id){
-    print_r($id.'进程退出了'.PHP_EOL);
+$pool->on('WorkerStop', function ($pool, $id) {
+    print_r($id . '进程退出了' . PHP_EOL);
 });
 $pool->start();
