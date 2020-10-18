328a4f12bfd56cef2ddd83eae6e82c14faf6b1f9
tsli <2816869721@qq.com>
2020-10-18 16:50:20 +0800

    ''

--- app/api/controller/Test.php
+++ app/api/controller/Test.php
@@ -17,20 +17,16 @@ class Test extends BaseController
             'cc'=>'require|mobile'
         ]);
         $pdo = $this->pdoPool->get();
-        $statement = $pdo->prepare('SELECT ? + ?');
+        $statement = $pdo->prepare('SELECT * FROM test');
         if (!$statement) {
             throw new RuntimeException('Prepare failed');
         }
-        $a = mt_rand(1, 100);
-        $b = mt_rand(1, 100);
         $result = $statement->execute([$a, $b]);
         if (!$result) {
             throw new RuntimeException('Execute failed');
         }
         $result = $statement->fetchAll();
-        if ($a + $b !== (int)$result[0][0]) {
-            throw new RuntimeException('Bad result');
-        }
+        print_r($result);
         $this->pdoPool->put($pdo);
         art_assign(200,json_encode($params));
 
