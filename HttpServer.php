938b37a11028c284291737d0a8639b7136e5d099
tsli <2816869721@qq.com>
2020-10-20 14:47:52 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -3,6 +3,7 @@
 namespace art;
 
 
+use art\db\DB;
 use art\exception\ClassNotFoundException;
 use art\exception\HttpException;
 use Swoole\Coroutine\Http\Server;
@@ -20,8 +21,9 @@ $pidPool->set(['enable_coroutine' => true]);
 $pidPool->on('workerStart', function ($pidPool, $id) {
     //每个进程都监听9501端口
     $server = new Server('0.0.0.0', '9502', false, true);
-    $server->handle('/', function (Request $request, Response $response) {
+    $server->handle('/', function (Request $request, Response $response) use($id) {
         //有优化空间 使用context来管理 变成单例，不用每次加载  已经处理，全部换成静态的方法了 cocomposer require --dev "eaglewu/swoole-ide-helper:dev-master"使用context管理上下文
+        echo '本次请求的进程ID'.$id;
         try {
             HttpApp::init($request, $response);
             HttpApp::run();
