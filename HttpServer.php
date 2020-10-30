d8219d0ecc8e61359c162a1dd7d54abaf00f1b99
tsli <2816869721@qq.com>
2020-10-30 14:40:54 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -26,7 +26,9 @@ $pidPool->on('workerStart', function ($pidPool, $id) {
     //每个进程都监听9501端口
     global $table;
     $table->set($id,['poolID',$id]);
-    var_dump($table);
+    foreach ($table as $row){
+        var_dump($row);
+    }
     $server = new Server('0.0.0.0', '9502', false, true);
     $server->handle('/', function (Request $request,Response $response) {
         //有优化空间 使用context来管理 变成单例，不用每次加载  已经处理，全部换成静态的方法了 cocomposer require --dev "eaglewu/swoole-ide-helper:dev-master"使用context管理上下文
