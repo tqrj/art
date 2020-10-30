f0af1d0b4c5383f0414fb4c8262ffa82af51a828
tsli <2816869721@qq.com>
2020-10-30 14:51:29 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -22,14 +22,14 @@ $table->create();
 $pidPool = new Pool(swoole_cpu_num() * 2);
 //让每个OnWorkerStart回调都自动创建一个协程
 $pidPool->set(['enable_coroutine' => true]);
-$pidPool->on('workerStart', function ($pidPool, $id) {
+$pidPool->on('workerStart', function ($pidPool,int $id) {
     //每个进程都监听9501端口
-    echo $id;
-    global $table;
-    $table->set($id,['poolID',$id]);
-    foreach ($table as $row){
-        var_dump($row);
-    }
+    print_r($id);
+//    global $table;
+//    $table->set($id,['poolID',$id]);
+//    foreach ($table as $row){
+//        var_dump($row);
+//    }
     $server = new Server('0.0.0.0', '9502', false, true);
     $server->handle('/', function (Request $request,Response $response) {
         //有优化空间 使用context来管理 变成单例，不用每次加载  已经处理，全部换成静态的方法了 cocomposer require --dev "eaglewu/swoole-ide-helper:dev-master"使用context管理上下文
