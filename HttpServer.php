5fe58e96496cbb2dd458da2acb6debc80f61fee3
tsli <2816869721@qq.com>
2020-10-31 13:06:22 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -15,16 +15,20 @@ use Swoole\Table;
 
 require 'vendor/autoload.php';
 
+//把消息放在table
+//key为当前进程ID
+//msg pool status
+//进程定时读当前进程的发完状态改为1，投递消息的时候看状态，如果待处理就等待
+//问题就是这个表要维护，尽量避免遍历
 
-$table = new Table(1024*5);
-$table->column('ws',Table::TYPE_STRING);
-
+ArtWs::init();
 //多进程管理模块
 $pidPool = new Pool(swoole_cpu_num() * 2);
 //让每个OnWorkerStart回调都自动创建一个协程
 $pidPool->set(['enable_coroutine' => true]);
 $pidPool->on('workerStart', function ($pidPool,int $id) {
-    //每个进程都监听9501端口
+    //通过 getProcess 然后创建子进程，然后监听
+    //每个进程都监听9502端口
     $server = new Server('0.0.0.0', '9502', false, true);
     $server->handle('/', function (Request $request,Response $response) {
         //有优化空间 使用context来管理 变成单例，不用每次加载  已经处理，全部换成静态的方法了 cocomposer require --dev "eaglewu/swoole-ide-helper:dev-master"使用context管理上下文
@@ -38,30 +42,29 @@ $pidPool->on('workerStart', function ($pidPool,int $id) {
             art_assign(404, $e->getMessage());
         }
     });
+
+    //websocket部分
+    ArtWs::initPool($id);
     $server->handle('/so',function (Request $request,Response $ws){
         $bool = $ws->upgrade();
         if ($bool == false){
             return;
         }
-        $wsId = getObjectId($ws);
-        global $table;
-        $table->set($wsId,['ws'=>serialize($ws)]);
+        ArtWs::setWs($ws);
         while (true){
             $frame = $ws->recv();
             if ($frame === ''){
                 echo '关闭了'.PHP_EOL;
+                ArtWs::delWs($ws);
                 $ws->close();
                 break;
             } else if ($frame === false) {
+                ArtWs::delWs($ws);
                 echo "error : " . swoole_last_error() . "\n";
                 break;
             } else {
-                foreach ($table as $item)
-                {
-                    $temp = unserialize($item['ws']);
-                    $temp->push("Server：{$frame->data}");
-                }
-                //$ws->push("Server：{$frame->data}");
+                ArtWs::pushMsgAll("Server：{$frame->data}");
+                //$ws->push();
             }
         }
 
---
+++ art/ArtWs.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace art;
+
+
+use Swoole\Http\Response;
+use Swoole\Table;
+use Swoole\Timer;
+
+class ArtWs
+{
+    private function __construct()
+    {
+
+    }
+
+    private function __clone()
+    {
+
+    }
+
+    private function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
+
+    private static Table $wsTable;
+
+    private static array $WsObject;
+
+    public static function init()
+    {
+        if (!empty(self::$wsTable)) {
+            return false;
+        }
+        self::$wsTable = new Table(1024);
+        self::$wsTable->column('msg', Table::TYPE_STRING, 1024 * 10);
+        self::$wsTable->column('status', Table::TYPE_INT);
+        self::$wsTable->create();
+
+        return true;
+    }
+
+    public static function initPool($poolId)
+    {
+        self::$wsTable->set($poolId, ['msg' => '', 'status' => 1]);
+        Timer::tick(10, function ($timerId, $poolId) {
+            $row = self::$wsTable->get($poolId);
+            if ($row['status'] == 1) {
+                return;
+            }
+            $msg = $row['msg'];
+            array_map(function (Response $ws) use ($msg) {
+                $ws->push($msg);
+            }, self::$WsObject);
+            $row['status'] = 1;
+            self::$wsTable->set($poolId,$row);
+        }, $poolId);
+    }
+
+    public static function setWs(Response $ws)
+    {
+        $wsId = getObjectId($ws);
+        self::$WsObject[$wsId] = $ws;
+    }
+
+    public static function delWs(Response $ws)
+    {
+        $wsId = getObjectId($ws);
+        unset(self::$WsObject[$wsId]);
+    }
+
+    public static function pushMsgAll(string $msg)
+    {
+        foreach (self::$wsTable  as $key=>$item){
+            $item['msg'] = $msg;
+            $item['status'] = 0;
+            self::$wsTable->set($key,$item);
+        }
+    }
+
+    public static function getWsTable(): Table
+    {
+        return self::$wsTable;
+    }
+
+    public static function getWsObjects():array
+    {
+        return self::$WsObject;
+    }
+}
