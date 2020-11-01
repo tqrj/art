1cd4f6e792e716b6e4cd2d3bafef65da9d8f8e53
tsli <2816869721@qq.com>
2020-11-01 16:04:19 +0800

    ''

--- test.php
+++ test.php
@@ -36,7 +36,7 @@ include 和require是有区别的 这个区别还没有测试，我不清楚incl
             $str[] = '单10';
             $str[] = '双50';
             $str[] = '万23456千23456除各1';
-            $rand = mt_rand(0,2);
+            $rand = mt_rand(2);
             $str = $str[$rand];
             $len  = pack('i',strlen($str)+4);
             $client->send($len.$str);
