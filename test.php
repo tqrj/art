f534e66aa53b1b391a7863b88f5e986a47fb267e
tsli <2816869721@qq.com>
2020-11-01 13:05:51 +0800

    ''

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ Co\run(function(){
 
     $n=0;
     while ($n < 1000){
-        \Co\System::sleep(0.1);
+        \Co\System::sleep(0.05);
         $n++;
         go(function (){
             $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
