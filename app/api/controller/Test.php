2847f989c9c3a9b48d6e5c147d3bfab144540a85
tsli <2816869721@qq.com>
2020-11-02 12:34:36 +0800

    ''

--- app/api/controller/Test.php
+++ app/api/controller/Test.php
@@ -22,11 +22,8 @@ class Test extends BaseController
 //        ]);
         $client = new Client(SWOOLE_SOCK_TCP);
         $client->set([
-            'open_length_check'     => true,
-            'package_max_length'    => 81920,
-            'package_length_type'   => 'l',
-            'package_length_offset' => 0,
-            'package_body_offset'   => 0,
+            'open_eof_check' => true,   //打开EOF检测
+            'package_eof'    => "\r\n", //设置EOF
         ]);
         if (!$client->connect('39.101.214.137', 9501))
         {
@@ -37,21 +34,17 @@ class Test extends BaseController
         $str[] = '单10';
         $str[] = '12345-12345-12345-12369-2580/0.1';
         $str[] = '万23456千23456除各1';
-        $rand = 1;
+        $rand = 2;
         $str = $str[$rand];
         $len  = pack('i',strlen($str)+4);
         $client->send($len.$str);
         //sleep(0.5);
         //}
-        while (true){
-            $result = $client->recv();
-            if ($result == false){
-                echo $client->errMsg;
-                $result = $client->errMsg;
-                break;
-            }
+        $result = $client->recv();
+        if ($result == false){
+            echo $client->errMsg;
+            $result = $client->errMsg;
         }
-
         $client->close();
         art_assign(200,urldecode($result));
     }
