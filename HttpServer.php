7447a99853552ca71cbfd5e63ab2d6e8e937e764
tsli <2816869721@qq.com>
2020-11-02 19:05:16 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -60,7 +60,7 @@ $pidPool->on('workerStart', function ($pidPool,int $id) {
                 echo "error : " . swoole_last_error() . "\n";
                 break;
             } elseif($frame->opcode == WEBSOCKET_OPCODE_TEXT){
-                ArtWs::pushMsg($frame->data,$wsId,'',$wsGroup);
+                ArtWs::pushMsg($frame->data,$wsId,-1,$wsGroup);
                 //$ws->push();
             }
         }
