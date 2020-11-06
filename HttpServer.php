81665dd653ba828e6f9e9b35f8b9dee7feef354d
tsli <2816869721@qq.com>
2020-11-06 12:57:21 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -23,9 +23,7 @@ $pidPool->on('workerStart', function ($Pool, int $id) {
     //通过 getProcess 然后创建子进程，然后监听
     //每个进程都监听9502端口
     $server = new Server('0.0.0.0', '80', false, true);
-    $server->set(['document_root' => '/home/hnim/public',
-        'enable_static_handler' => true,
-        ]);
+    $server->set(["static_handler_locations" => ['/static', '/static']]);
     $server->handle('/', function (Request $request, Response $response) {
         //有优化空间 使用context来管理 变成单例，不用每次加载  已经处理，全部换成静态的方法了 cocomposer require --dev "eaglewu/swoole-ide-helper:dev-master"使用context管理上下文
         try {
