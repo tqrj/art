ea98bc8503dd915e7db2293ab7e6e1ec9ceb48ec
tsli <2816869721@qq.com>
2020-11-08 19:55:54 +0800

    ''

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 
+use app\traits\Lottery;
+
 Co\run(function () {
 
 
@@ -7,37 +9,8 @@ Co\run(function () {
     while ($n < 100000) {
         \Co\System::sleep(0.02);
         $n++;
-        go(function () {
-            $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
-            $client->set(array(
-                'open_length_check' => true,
-                'package_max_length' => 1024*30,
-                'package_length_type' => 'l',
-                'package_length_offset' => 0,
-                'package_body_offset' => 0,
-            ));
-            if (!$client->connect('39.101.214.137', 9501)) {
-                echo "connect failed. Error: {$client->errCode}\n";
-                return;
-            }
-            $str = [];
-            $str[] = '单10';
-            $str[] = '12345-12345-12345-12369-2580/0.1';
-            $str[] = '万23456千23456除各1';
-            $rand = mt_rand(0, 2);
-            $str = urlencode($str[$rand]);
-            $len = pack('i', strlen($str) + 4);
-            $client->send($len . $str);
-            //sleep(0.5);
-            //}
-            $result = $client->recv();
-            if ($result == false) {
-                echo $client->errMsg;
-            } else {
-                echo urldecode(mb_substr($result, 4)) . PHP_EOL;
-            }
-            $client->close();
-        });
+        $result = Lottery::getCode(Lottery::LOTTERY_TYPE_OLD);
+        echo $result.PHP_EOL;
     }
     echo 'ojbk处理完毕'.PHP_EOL;
 });
