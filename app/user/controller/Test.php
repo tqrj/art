c99071d6c63a95a1254048f3eba54550b2c5b1e3
tsli <2816869721@qq.com>
2020-11-08 16:02:42 +0800

    ''

--- app/user/controller/Test.php
+++ app/user/controller/Test.php
@@ -99,7 +99,7 @@ class Test extends BaseController
             $result = $client->errMsg;
         }
         $client->close();
-        $result = urldecode(gzuncompress(mb_substr($result,8)));
+        $result = urldecode(mb_substr($result,4));
         echo $result;
         var_dump(json_decode($result,true));
         art_assign(200,'success',json_decode($result,true));
