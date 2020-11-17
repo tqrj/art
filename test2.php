2f6782659e35d9435c04ff338c9c0a74316e5ad2
tsli <2816869721@qq.com>
2020-11-17 11:50:20 +0800

    ''

--- test2.php
+++ test2.php
@@ -1,23 +1,26 @@
 <?php
+
+echo 30%2;
+
 //echo  PACK('H*','696D616765732F646564652E6A7067');
-$data = ['qq','ww','ee','cc'];
-//unset($data[0]);
-art_unset('qq',$data);
-var_dump(json_encode($data));
-/**
- * 针对无键名数组unset 避免jsonEncode变成对象
- * @param $value
- * @param $array
- * @return array|bool
- */
-function art_unset($value,&$array)
-{
-    $sort = array_flip($array);
-    if (isset($sort[$value])){
-        return array_splice($array,$sort[$value],1);
-    }
-    return false;
-}
+//$data = ['qq','ww','ee','cc'];
+////unset($data[0]);
+//art_unset('qq',$data);
+//var_dump(json_encode($data));
+///**
+// * 针对无键名数组unset 避免jsonEncode变成对象
+// * @param $value
+// * @param $array
+// * @return array|bool
+// */
+//function art_unset($value,&$array)
+//{
+//    $sort = array_flip($array);
+//    if (isset($sort[$value])){
+//        return array_splice($array,$sort[$value],1);
+//    }
+//    return false;
+//}
 
 
 
