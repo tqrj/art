dd13307b169ab7510ed2976163b44875d327cc06
tsli <2816869721@qq.com>
2020-11-17 16:13:54 +0800

    ''

--- HttpServer.php
+++ HttpServer.php
@@ -73,6 +73,7 @@ $pidPool->on('workerStart', function ($Pool, int $id) {
                     _art_assign_ws(404, $e->getMessage(),[],0,$wsId);
                     WsApp::end();
                     $ws->close();
+                    break;
                 }
                 //ArtWs::pushMsg($frame->data,$wsId,2);
                 //$ws->push();
