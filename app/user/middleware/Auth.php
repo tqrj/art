f9d072fd0e9a82c0618bc4f19e5f9f98e9711909
tsli <2816869721@qq.com>
2020-12-05 17:06:40 +0800

    ''

--- app/user/middleware/Auth.php
+++ app/user/middleware/Auth.php
@@ -47,7 +47,7 @@ class Auth
         $result = $medoo->get('user(u)',
             [
                 '[><]user_quantity(q)'=>['u.id'=>'user_id'],
-                '[><]agent(a)'=>['agent_id'=>'a.id'],
+                '[><]agent(a)'=>['q.agent_id'=>'id'],
             ],
             [
                 'u.id',
