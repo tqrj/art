3741aa8fa17b3ed8dd539079a7899663fe5be54d
tsli <2816869721@qq.com>
2020-12-17 18:29:51 +0800

    ''

--- app/agent/model/service/ProfitService.php
+++ app/agent/model/service/ProfitService.php
@@ -55,7 +55,7 @@ class ProfitService
         if (!$applyInfo){
             art_assign(202,'数据异常');
         }
-        $agentInfo['type'] == 1?$mark='上分请求处理通过':$mark='下分请求处理通过';
+        $applyInfo['type'] == 1?$mark='上分请求处理通过':$mark='下分请求处理通过';
         $applyInfo['quantity'] = $applyInfo['type'] == 1?abs($applyInfo['quantity']):-$applyInfo['quantity'];
         $medoo->beginTransaction();
         try {
