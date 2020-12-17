fe7de24d0fed2c017a4e64d876ced7ea6433ba12
tsli <2816869721@qq.com>
2020-12-17 20:20:28 +0800

    ''

--- app/agent/model/service/ProfitService.php
+++ app/agent/model/service/ProfitService.php
@@ -19,12 +19,12 @@ class ProfitService
     {
         $agentInfo = Context::get('authInfo');
         $medoo = new Medoo();
-        $result = $medoo->select('points','*',[
-            'agent_id'=>$agentInfo['id'],
-            'status'=>0,
-            'type'=>1,
-            'LIMIT'=>[$params['page'],$params['limit']],
-            'ORDER'=>['id'=>'DESC']
+        $result = $medoo->select('points', '*', [
+            'agent_id' => $agentInfo['id'],
+            'status' => 0,
+            'type' => 1,
+            'LIMIT' => [$params['page'], $params['limit']],
+            'ORDER' => ['id' => 'DESC']
         ]);
         return $result;
     }
@@ -33,12 +33,12 @@ class ProfitService
     {
         $agentInfo = Context::get('authInfo');
         $medoo = new Medoo();
-        $result = $medoo->select('points','*',[
-            'agent_id'=>$agentInfo['id'],
-            'status'=>0,
-            'type'=>-1,
-            'LIMIT'=>[$params['page'],$params['limit']],
-            'ORDER'=>['id'=>'DESC']
+        $result = $medoo->select('points', '*', [
+            'agent_id' => $agentInfo['id'],
+            'status' => 0,
+            'type' => -1,
+            'LIMIT' => [$params['page'], $params['limit']],
+            'ORDER' => ['id' => 'DESC']
         ]);
         return $result;
     }
@@ -47,38 +47,38 @@ class ProfitService
     {
         $medoo = new Medoo();
         $agentInfo = Context::get('authInfo');
-        $applyInfo = $medoo->get('points','*',[
-            'id'=>$params['id'],
-            'agent_id'=>$agentInfo['id'],
-            'status'=>0
+        $applyInfo = $medoo->get('points', '*', [
+            'id' => $params['id'],
+            'agent_id' => $agentInfo['id'],
+            'status' => 0
         ]);
-        if (!$applyInfo){
-            art_assign(202,'数据异常');
+        if (!$applyInfo) {
+            art_assign(202, '数据异常');
         }
-        $applyInfo['type'] == 1?$mark='上分请求处理通过':$mark='下分请求处理通过';
-        $applyInfo['quantity'] = $applyInfo['type'] == 1?abs($applyInfo['quantity']):-$applyInfo['quantity'];
+        $applyInfo['type'] == 1 ? $mark = '上分请求处理通过' : $mark = '下分请求处理通过';
+        $applyInfo['quantity'] = $applyInfo['type'] == 1 ? abs($applyInfo['quantity']) : -$applyInfo['quantity'];
         $medoo->beginTransaction();
         try {
-            QuantityLogService::push($applyInfo['user_id'],$applyInfo['agent_id'],$applyInfo['quantity'],$mark);
-            $pdoDoc = $medoo->update('user_quantity',['quantity[+]'=>$applyInfo['quantity']],[
-                'user_id'=>$applyInfo['user_id'],
-                'agent_id'=>$agentInfo['id']
+            QuantityLogService::push($applyInfo['user_id'], $applyInfo['agent_id'], $applyInfo['quantity'], $mark);
+            $pdoDoc = $medoo->update('user_quantity', ['quantity[+]' => $applyInfo['quantity']], [
+                'user_id' => $applyInfo['user_id'],
+                'agent_id' => $agentInfo['id']
             ]);
-            if (!$pdoDoc->rowCount()){
+            if (!$pdoDoc->rowCount()) {
                 throw new \Exception($pdoDoc->errorInfo());
             }
-            $pdoDoc = $medoo->update('points',['status'=>1],[
-                'id'=>$params['id'],
-                'agent_id'=>$agentInfo['id'],
-                'status'=>0
+            $pdoDoc = $medoo->update('points', ['status' => 1], [
+                'id' => $params['id'],
+                'agent_id' => $agentInfo['id'],
+                'status' => 0
             ]);
-            if (!$pdoDoc->rowCount()){
+            if (!$pdoDoc->rowCount()) {
                 throw new \Exception('更新错误');
             }
             $medoo->commit();
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             $medoo->rollBack();
-            art_assign(202,$e->getMessage());
+            art_assign(202, $e->getMessage());
         }
         return [];
     }
@@ -87,28 +87,28 @@ class ProfitService
     {
         $medoo = new Medoo();
         $agentInfo = Context::get('authInfo');
-        $applyInfo = $medoo->get('points','*',[
-            'id'=>$params['id'],
-            'agent_id'=>$agentInfo['id'],
-            'status'=>0
+        $applyInfo = $medoo->get('points', '*', [
+            'id' => $params['id'],
+            'agent_id' => $agentInfo['id'],
+            'status' => 0
         ]);
-        if (!$applyInfo){
-            art_assign(202,'数据异常');
+        if (!$applyInfo) {
+            art_assign(202, '数据异常');
         }
         $medoo->beginTransaction();
         try {
-            $pdoDoc = $medoo->update('points',['status'=>1,'update_time'=>art_d()],[
-                'id'=>$params['id'],
-                'agent_id'=>$agentInfo['id'],
-                'status'=>0
+            $pdoDoc = $medoo->update('points', ['status' => 1, 'update_time' => art_d()], [
+                'id' => $params['id'],
+                'agent_id' => $agentInfo['id'],
+                'status' => 0
             ]);
-            if (!$pdoDoc->rowCount()){
+            if (!$pdoDoc->rowCount()) {
                 throw new \Exception('更新错误');
             }
             $medoo->commit();
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             $medoo->rollBack();
-            art_assign(202,$e->getMessage());
+            art_assign(202, $e->getMessage());
         }
         return [];
     }
@@ -121,50 +121,50 @@ class ProfitService
     {
         $agentInfo = Context::get('authInfo');
         $medoo = new Medoo();
-        $max = $medoo->max('order','create_time',['agent_id'=>$agentInfo['id']]);
-        $min = $medoo->min('order','create_time',['agent_id'=>$agentInfo['id']]);
-        $maxCar = Carbon::parse($max,'Asia/Shanghai');
-        $minCar = Carbon::parse($min,'Asia/Shanghai');
+        $max = $medoo->max('order', 'create_time', ['agent_id' => $agentInfo['id']]);
+        $min = $medoo->min('order', 'create_time', ['agent_id' => $agentInfo['id']]);
+        $maxCar = Carbon::parse($max, 'Asia/Shanghai');
+        $minCar = Carbon::parse($min, 'Asia/Shanghai');
         $betArt = [];
         $result = [];
-        $n=0;
-        do{
+        $n = 0;
+        do {
             $betArt[$n][0] = $minCar->toDateTimeString();
             $minCar->addDays(7);
             //如果第一次匹配或者最后一次匹配 开始时间加7之后已经等于大于了 那么就以结束时间加一天为准。
-            $minCar->lt($maxCar)? $betArt[$n][1] = $minCar->toDateTimeString():$betArt[$n][1] = $maxCar->addDay()->toDateTimeString();
-            $n+=1;
-        }while($minCar->lt($maxCar));
-        array_walk($betArt,function ($item,$key,Medoo $medoo) use (&$result,$agentInfo){
-            $result[$key]['orderCount'] = $medoo->count('order','id',[
-                'agent_id'=>$agentInfo['id'],
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $minCar->lt($maxCar) ? $betArt[$n][1] = $minCar->toDateTimeString() : $betArt[$n][1] = $maxCar->addDay()->toDateTimeString();
+            $n += 1;
+        } while ($minCar->lt($maxCar));
+        array_walk($betArt, function ($item, $key, Medoo $medoo) use (&$result, $agentInfo) {
+            $result[$key]['orderCount'] = $medoo->count('order', 'id', [
+                'agent_id' => $agentInfo['id'],
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['playCodeCount'] = $medoo->sum('order','play_code_count',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['playCodeCount'] = $medoo->sum('order', 'play_code_count', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['quantityCount'] = $medoo->sum('order','quantity',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['quantityCount'] = $medoo->sum('order', 'quantity', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['flyQuantityRetCount'] = $medoo->sum('order','fly_quantity_ret',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['flyQuantityRetCount'] = $medoo->sum('order', 'fly_quantity_ret', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['locQuantityRetCount'] = $medoo->sum('order','loc_quantity_ret',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['locQuantityRetCount'] = $medoo->sum('order', 'loc_quantity_ret', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['timeStartShow'] = date('n-j',strtotime($item[0]));
+            $result[$key]['timeStartShow'] = date('n-j', strtotime($item[0]));
             $result[$key]['timeStart'] = $item[0];
-            $result[$key]['timeEndShow'] = date('n-j',strtotime($item[1]));
+            $result[$key]['timeEndShow'] = date('n-j', strtotime($item[1]));
             $result[$key]['timeEnd'] = $item[1];
-        },$medoo);
+        }, $medoo);
         return $result;
     }
 
@@ -177,48 +177,49 @@ class ProfitService
     {
         $medoo = new Medoo();
         $agentInfo = Context::get('authInfo');
-        $minCar = Carbon::parse($params['timeStart'],'Asia/Shanghai');
-        $maxCar = Carbon::parse($params['timeEnd'],'Asia/Shanghai');
+        $minCar = Carbon::parse($params['timeStart'], 'Asia/Shanghai');
+        $maxCar = Carbon::parse($params['timeEnd'], 'Asia/Shanghai');
         $betArt = [];
         $result = [];
-        $n=0;
-        do{
+        $n = 0;
+        do {
             $betArt[$n][0] = $minCar->toDateTimeString();
             $minCar->addDay();
+
             //如果第一次匹配或者最后一次匹配 开始时间加7之后已经等于大于了 那么就以结束时间加一天为准。
-            $minCar->lt($maxCar)? $betArt[$n][1] = $minCar->toDateTimeString():$betArt[$n][1] = $maxCar->addDay()->toDateTimeString();
-            $n+=1;
-        }while($minCar->lt($maxCar));
-        array_walk($betArt,function ($item,$key,Medoo $medoo) use (&$result,$agentInfo){
-            $result[$key]['orderCount'] = $medoo->count('order','id',[
-                'agent_id'=>$agentInfo['id'],
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $betArt[$n][1] = $minCar->isSameDay($maxCar) ? $maxCar->toDateTimeString():$minCar->toDateTimeString();
+            $n += 1;
+        } while ($minCar->lt($maxCar));
+        array_walk($betArt, function ($item, $key, Medoo $medoo) use (&$result, $agentInfo) {
+            $result[$key]['orderCount'] = $medoo->count('order', 'id', [
+                'agent_id' => $agentInfo['id'],
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['playCodeCount'] = $medoo->sum('order','play_code_count',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['playCodeCount'] = $medoo->sum('order', 'play_code_count', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['quantityCount'] = $medoo->sum('order','quantity',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['quantityCount'] = $medoo->sum('order', 'quantity', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['flyQuantityRetCount'] = $medoo->sum('order','fly_quantity_ret',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['flyQuantityRetCount'] = $medoo->sum('order', 'fly_quantity_ret', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['locQuantityRetCount'] = $medoo->sum('order','loc_quantity_ret',[
-                'agent_id'=>$agentInfo['id'],
-                'status'=>1,
-                'create_time[<>]'=>[$item[0],$item[1]]
+            $result[$key]['locQuantityRetCount'] = $medoo->sum('order', 'loc_quantity_ret', [
+                'agent_id' => $agentInfo['id'],
+                'status' => 1,
+                'create_time[<>]' => [$item[0], $item[1]]
             ]);
-            $result[$key]['timeStartShow'] = date('n-j',strtotime($item[0]));
+            $result[$key]['timeStartShow'] = date('n-j', strtotime($item[0]));
             $result[$key]['timeStart'] = $item[0];
-            $result[$key]['timeEndShow'] = date('n-j',strtotime($item[1]));
+            $result[$key]['timeEndShow'] = date('n-j', strtotime($item[1]));
             $result[$key]['timeEnd'] = $item[1];
-        },$medoo);
+        }, $medoo);
         return $result;
     }
 }
