feb3612de64ee93c4e80d971dbe98140e5c480b6
tsli <2816869721@qq.com>
2020-12-31 12:53:46 +0800

    ''

--- app/agent/model/service/RoomService.php
+++ app/agent/model/service/RoomService.php
@@ -226,7 +226,7 @@ class RoomService
             $whetherScore = self::_whetherScore($lotteryCode, $orderInfo['play_code'], $orderInfo['play_site'], $orderInfo['single_quantity'], $orderInfo['line']);
             if ($whetherScore[0] == false) {
                 //没中奖直接滚蛋
-                $medoo->update('order', ['status' => 1, 'update' => 1], ['id' => $orderInfo['id']]);
+                $medoo->update('order', ['status' => 1, 'update' => art_d()], ['id' => $orderInfo['id']]);
                 return;
             }
             $medoo->beginTransaction();
